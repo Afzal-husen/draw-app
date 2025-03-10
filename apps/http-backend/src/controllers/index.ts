d3fe36cf6774fb40644c58be0e475b0a03a01f86
Afzal Diwan <afzal@relso.today>
2025-03-11 00:27:12 +0530

    wip: http-backend

--- apps/http-backend/src/controllers/index.ts
+++ apps/http-backend/src/controllers/index.ts
@@ -1,13 +1,32 @@
 import { RequestHandler } from "express";
 import { createRoomSchema, signinSchema, signupSchema } from "@repo/common";
 import { CustomError } from "../middlewares/custom-error";
+import { prisma } from "@repo/db";
+import bcryt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common";
 
-const signup: RequestHandler = (req, res, next) => {
+const signup: RequestHandler = async (req, res, next) => {
   try {
     const parsedBody = signupSchema.safeParse(req.body);
     if (!parsedBody.success) {
       return next(new CustomError(400, parsedBody.error.message));
     }
+
+    const hashedPassword = await bcryt.hash(parsedBody.data.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name: parsedBody.data.userName,
+        password: hashedPassword,
+        email: parsedBody.data.email,
+      },
+    });
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+    res
+      .status(201)
+      .json({ error: false, message: "User created successfully" });
   } catch (error) {
     next(error);
   }
